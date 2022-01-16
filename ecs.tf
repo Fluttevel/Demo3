e70664af540e74c09c24363ac3c02a35407412c2
Fluttevel <fluttevel@gmail.com>
2022-01-16 23:20:05 +0200

    Init

--- ecs.tf
+++ ecs.tf
@@ -1,18 +1,18 @@
-# =========| ECR |=========
+# ==========| ECR |==========
 
 resource "aws_ecr_repository" "ecr_repository" {
   name = local.repository_name
 }
 
 
-# =========| ECS |=========
+# ==========| ECS |==========
 
 resource "aws_ecs_cluster" "main" {
   name = "${var.app_name}-${var.environment}-cluster"
 }
 
-data "template_file" "cb_app" {
-  template = file(var.taskdef_template)
+data "template_file" "container_definitions" {
+  template = file(var.task_definition_template)
 
   vars = {
     app_image      = local.app_image
@@ -20,7 +20,7 @@ data "template_file" "cb_app" {
     fargate_cpu    = var.fargate_cpu
     fargate_memory = var.fargate_memory
     aws_region     = var.aws_region
-    env            = var.environment
+    environment    = var.environment
     app_name       = var.app_name
     image_tag      = var.image_tag
   }
@@ -30,24 +30,12 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "${var.app_name}-${var.environment}-task"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
+  network_mode             = var.network_mode
+  requires_compatibilities = [var.launch_type]
   cpu                      = var.fargate_cpu
   memory                   = var.fargate_memory
-  container_definitions = jsonencode([{
-    name        = "first"
-    image       = "692807581431.dkr.ecr.eu-west-2.amazonaws.com/utilities-dev:v.1"
-    essential   = true
-    cpu         = 256
-    memory      = 512
 
-    portMappings = [
-      {
-        containerPort = 80
-        hostPort      = 80
-      }
-    ]
-  }])
+  container_definitions    = data.template_file.container_definitions.rendered
 }
 
 resource "aws_ecs_service" "main" {
@@ -55,7 +43,7 @@ resource "aws_ecs_service" "main" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = var.app_count
-  launch_type     = "FARGATE"
+  launch_type     = var.launch_type
 
   network_configuration {
     security_groups  = [aws_security_group.alb-security-group.id]
@@ -65,7 +53,7 @@ resource "aws_ecs_service" "main" {
 
   load_balancer {
     target_group_arn = aws_lb_target_group.asg.id
-    container_name   = "first"
+    container_name   = var.container_name
     container_port   = var.app_port
   }
 
